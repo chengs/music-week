6302f280cabfa5155c67d8a7f42037dbdfada815
Chen Guangshuo <cgs.sjtu@gmail.com>
2013-06-25 20:45:32 +0800

    最近修改

--- wp-content/themes/musicweek/event.php
+++ wp-content/themes/musicweek/event.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    Template Name: 活动日程模板
+    Template Name: 活动模板
 */
 get_header();
 ?>
@@ -8,11 +8,12 @@ get_header();
         <?php while (have_posts()) : the_post(); ?>
             <div class="content">
                 <div class="nav">
-                    <span class="cat">Schedule-日程</span>
+                    <span class="cat">Event-活动</span>
                 </div>
                 <div class="article">
                     <div class="text">
                         <article>
+                            <h1><?php the_title();?></h1>
                             <?php the_content(); ?>
                         </article>
                     </div>
