b520ea0caf207f8833f64be50a9a8aa96ab5aaa5
chen guangshuo <cgs.sjtu@gmail.com>
2013-09-05 10:06:53 +0200

    Footer效果修复

--- wp-content/themes/musicweek/footer.php
+++ wp-content/themes/musicweek/footer.php
@@ -1,4 +1,29 @@
-<footer></footer>
+<footer>
+    <div class="followUs">
+        <p class="title">
+            关注我们
+        </p>
+        <p>
+            <a href="http://weibo.com/2011emw" target="_blank">新浪微博</a>
+        </p>
+        <p>
+            <a href="http://site.douban.com/125432/" target="_blank">豆瓣小站</a>
+        </p>
+    </div>
+    <div class="contactUs">
+        <p class="title">
+            联系方式
+        </p>
+        <p>电话/传真：（021）64318542</p>
+        <p>电子邮箱： emwshanghai@gmail.com</p>
+        <p>地址：上海市汾阳路20号上海音乐学院音乐工程系 &nbsp; &nbsp;2013上海音乐学院国际电子音乐周组委会</p>
+        <p>邮编：200031</p>
+    </div>
+    
+    <p class="copyright">
+        &copy; Copyright 2013-2014 上海音乐学院
+    </p>
+</footer>
 
 <?php wp_footer();?>
 </body>
